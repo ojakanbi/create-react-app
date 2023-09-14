4d4336ee313dab3fbfd710a8b55b620ee3c8d0c8
Oyindamola J Akanbi <ojakanbi@MacBook-Air.local>
2023-09-14 14:54:43 -0400

    hw week4

--- src/App.js
+++ src/App.js
@@ -3,23 +3,39 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div>
+    {/* <button id="cloneButton" onClick={handleCloneCard}>Clone Card</button>
+    <button id="colorButton" onClick={handleChangeCardColor}>Change Card Color</button>
+    <button id="titleButton" onClick={handleChangeTitle}>Change Title</button>
+    <button id="deleteButton" onClick={handleDeleteLastCard}>Delete Last Card</button> */}
+
+    <div className="cards">
+      {/* Card Template */}
+      <div className="card-container">
+        <div className="card-content">
+          {/* Card Title */}
+          <h2 className="cardTitle">PSU Blue and White</h2>
+          {/* Card Description */}
+          <p> "Blue and White is a Penn State Football Game day where the team is split into two teams blue and white" : "Blue and White game day"</p>
+          {/* Card Image */}
+          <img src="https://nittanylionswire.usatoday.com/wp-content/uploads/sites/100/2023/04/USATSI_20469545.jpg?w=1000" alt="Card Image" />
+          <div>
+            {/* Toggle Description Button */}
+            <button className="details-button">Toggle Description</button>
+          </div>
+          {/* Hidden Description */}
+         <p className="description-hidden">Blue and White is a Penn State Football Game day where the team is split into two teams blue and white</p>
+        </div>
+      </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
+
+
+
+
+
